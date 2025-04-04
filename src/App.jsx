a88d1046033dc132311d450337f298e3ea155a3c
Karimnazarov Jamshed <karimnazarovj@gmail.com>
2025-04-03 21:35:25 -0500

    implemneted sign in with firebase

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Navigate } from "react-router-dom";
-import NavBar from './components/NavBar'; // Import the NavBar component
-import Home from './pages/Home';
-import FindBook from './pages/FindBook';
-import SellBook from './pages/SellBook';
-import Messages from './pages/Messages';
+import NavBar from "./components/NavBar"; // Import the NavBar component
+import Home from "./pages/Home";
+import FindBook from "./pages/FindBook";
+import SellBook from "./pages/SellBook";
+import Messages from "./pages/Messages";
+import SignUp from "./pages/SignUp";
 
 const App = () => {
   return (
@@ -16,10 +17,10 @@ const App = () => {
         <Route path="/find" element={<FindBook />} />
         <Route path="/sell" element={<SellBook />} />
         <Route path="/messages" element={<Messages />} />
+        <Route path="/signup" element={<SignUp />} />
       </Routes>
     </Router>
   );
 };
 
 export default App;
-

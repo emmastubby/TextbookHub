d6d9ec7769e370b4080e28934413a2a81ebc5d16
Karimnazarov Jamshed <karimnazarovj@gmail.com>
2025-04-08 19:12:47 -0500

    sell book page loads data from db

--- src/components/SellBookCard.jsx
+++ src/components/SellBookCard.jsx
@@ -1,30 +1,48 @@
-import { IconButton } from '@mui/material';
-import { Edit } from '@mui/icons-material';
-import { FavoriteBorder } from '@mui/icons-material';
+import { IconButton } from "@mui/material";
+import { Edit } from "@mui/icons-material";
+import { FavoriteBorder } from "@mui/icons-material";
 
-const SellBookCard = ({picture, title, edition, author, price, condition}) => {
-    return (
-      <div className="w-80 rounded-lg shadow-lg overflow-hidden bg-white mt-4">
-        <img src={picture} className="w-full h-48 object-cover" />
-        <div className="p-4 pb-2">
-          <h2 className="text-xl font-bold">{title}</h2>
-          <p className="text-gray-500 mt-1">{edition}th edition</p>
-          <p className="text-gray-500 mt-1">by {author}</p>
-
-          <p className="font-bold mt-2">Asking Price: ${price}</p>
-          <hr />
-          <p className="text-gray-500 mt-1">{condition} condition</p>
-        </div>
-        <div className="p-4 pt-2">
-          <button
-            className="w-full bg-yellow-500 text-white py-2 px-4 mr-1 rounded-lg hover:bg-yellow-600 transition"
-          >
-            Edit Listing
-            <Edit className="ml-4 mb-1"></Edit>
-          </button>
-        </div>
-      </div>
-    );
+const SellBookCard = ({
+  picture,
+  title,
+  edition,
+  author,
+  price,
+  condition,
+}) => {
+  // edition is a number, convert to string. If 1st, 2nd, 3rd, else add "th"
+  const editionString = (edition) => {
+    if (edition === 1) {
+      return "1st";
+    } else if (edition === 2) {
+      return "2nd";
+    } else if (edition === 3) {
+      return "3rd";
+    } else {
+      return `${edition}th`;
+    }
   };
 
-  export default SellBookCard;
+  return (
+    <div className="w-80 rounded-lg shadow-lg overflow-hidden bg-white mt-4">
+      <img src={picture} className="w-full h-48 object-cover" />
+      <div className="p-4 pb-2">
+        <h2 className="text-xl font-bold">{title}</h2>
+        <p className="text-gray-500 mt-1">{editionString(edition)}</p>
+        <p className="text-gray-500 mt-1">by {author}</p>
+
+        <p className="font-bold mt-2">Asking Price: ${price}</p>
+        <hr />
+        <p className="text-gray-500 mt-1">{condition} condition</p>
+      </div>
+      <div className="p-4 pt-2">
+        <button className="w-full bg-yellow-500 text-white py-2 px-4 mr-1 rounded-lg hover:bg-yellow-600 transition">
+          Edit Listing
+          <Edit className="ml-4 mb-1"></Edit>
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default SellBookCard;

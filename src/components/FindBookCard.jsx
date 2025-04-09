fc81a0b7a3bd175bd0aa753bd29d44224529ba09
emmastubby <98038103+emmastubby@users.noreply.github.com>
2025-04-08 19:45:18 -0500

    make book cards sideways scrollable

--- src/components/FindBookCard.jsx
+++ src/components/FindBookCard.jsx
@@ -14,12 +14,25 @@ const FindBookCard = ({ picture, title, edition, author, price, condition }) => 
         setIsFavorited((prev) => !prev);
     };
 
+    // edition is a number, convert to string. If 1st, 2nd, 3rd, else add "th"
+    const editionString = (edition) => {
+        if (edition === 1) {
+            return "1st";
+        } else if (edition === 2) {
+            return "2nd";
+        } else if (edition === 3) {
+            return "3rd";
+        } else {
+            return `${edition}th`;
+        }
+    };
+
     return (
         <div className="min-w-[320px] w-[320px] max-w-[320px] min-h-[429px] h-[429px] rounded-lg shadow-lg overflow-hidden bg-white mt-4">
             <img src={picture} className="w-full h-48 object-cover" />
             <div className="p-4 pb-2">
                 <h2 className="text-xl font-bold">{title}</h2>
-                <p className="text-gray-500 mt-1">{edition}th edition</p>
+                <p className="text-gray-500 mt-1">{editionString(edition)} edition</p>
                 <p className="text-gray-500 mt-1">by {author}</p>
 
                 <p className="font-bold mt-2">Asking Price: ${price}</p>

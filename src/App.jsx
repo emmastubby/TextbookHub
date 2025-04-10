0088e3303d6df6cb3726fe30735879f9c5493372
Isaac Trevino <isaac.j.trevino-1@ou.edu>
2025-04-10 17:38:25 -0500

    Added login check to routes

--- src/App.jsx
+++ src/App.jsx
@@ -15,8 +15,13 @@ import SignUp from "./pages/SignUp";
 import Login from "./pages/Login";
 import ListBookForm from "./components/ListBookForm";
 import EditBook from "./pages/EditBook";
+import { useRecoilValue } from "recoil";
+import { authState } from "./recoil/atoms";
+
 
 const App = () => {
+  const auth = useRecoilValue(authState);
+
   const location = useLocation();
   const hideNav = ["/signup", "/login"].includes(location.pathname);
 
@@ -24,15 +29,46 @@ const App = () => {
     <>
       {!hideNav && <NavBar />}
       <Routes>
-        <Route path="/" element={<Navigate to="/find" />} />
-        <Route path="/find" element={<FindBook />} />
-        <Route path="/sell" element={<SellBook />} />
-        <Route path="/sell/new" element={<ListBookForm />} />
-        <Route path="/sell/edit" element={<EditBook />} />
-        <Route path="/messages" element={<Messages />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
+  <Route
+    path="/"
+    element={
+      auth.isLoggedIn ? <Navigate to="/find" /> : <Navigate to="/login" />
+    }
+  />
+  <Route
+  path="/home"
+  element={auth.isLoggedIn ? <Home /> : <Navigate to="/login" />}
+  />
+  <Route
+    path="/find"
+    element={auth.isLoggedIn ? <FindBook /> : <Navigate to="/login" />}
+  />
+  <Route
+    path="/sell"
+    element={auth.isLoggedIn ? <SellBook /> : <Navigate to="/login" />}
+  />
+  <Route
+    path="/sell/new"
+    element={auth.isLoggedIn ? <ListBookForm /> : <Navigate to="/login" />}
+  />
+  <Route
+    path="/sell/edit"
+    element={auth.isLoggedIn ? <EditBook /> : <Navigate to="/login" />}
+  />
+  <Route
+    path="/messages"
+    element={auth.isLoggedIn ? <Messages /> : <Navigate to="/login" />}
+  />
+  <Route
+    path="/signup"
+    element={auth.isLoggedIn ? <Navigate to="/find" /> : <SignUp />}
+  />
+  <Route
+    path="/login"
+    element={auth.isLoggedIn ? <Navigate to="/find" /> : <Login />}
+  />
+</Routes>
+
     </>
   );
 };
